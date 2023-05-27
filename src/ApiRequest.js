bc8c6c8c4d0b98309070c314bf84df2057443707
unknown <alessandro.bmb@icloud.com>
2023-05-28 00:16:21 +0200

    a

--- src/ApiRequest.js
+++ src/ApiRequest.js
@@ -1,6 +1,7 @@
 import {useEffect,useState} from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import {userProfile} from "./Pages/UserProfile.js";
+import "./userTable.css";
 
 
 const ApiComponent= () => {
@@ -12,56 +13,45 @@ const ApiComponent= () => {
       .then(response => response.json())
       .then(json => setUserProfiles(json))
       .catch(error => console.error(error));
-  }, []);
+    }, []);
 
 
-  const navigateToEdit = ()=>{
-    //navigate('/userProfile');
-  }
+    const navigateToEdit = ()=>{
+      //navigate('/userProfile');
+      }
+      
     
-  
-  
-    return userProfiles.map((userProfiles, index) => {
-  
-      return (
-        <tr>
-      <td class="pl-4">{index+1}</td>
-      <td>
-          <h5 class="font-medium mb-0">{userProfiles.name} {userProfiles.surname}</h5>
-          <span>{userProfiles.address.street} {userProfiles.address.city} </span>
-      </td>
-      <td>
-          <span>{userProfiles.rank} </span><br></br>
-          
-      </td>
-      <td>
-          <span>{userProfiles.email}</span><br></br>
-          <span>{userProfiles.telephoneNumber} </span>
-      </td>
-      <td>
-          <span>{userProfiles.birthDate.split("T")[0].split("-")[2]}-{userProfiles.birthDate.split("T")[0].split("-")[1]}-{userProfiles.birthDate.split("T")[0].split("-")[0]}</span><br></br>
-          <span></span>
-      </td>
-      <td>
-        <select class="form-control category-select" id="exampleFormControlSelect1">
-          <option>Modulator</option>
-          <option>Admin</option>
-          <option>User</option>
-          <option>Subscriber</option>
-        </select>
-      </td>
-      <td>
-        <button type="button" class="btn btn-outline-info btn-circle btn-lg btn-circle"><i class="fa fa-key"></i> </button>
-        <button type="button" class="btn btn-outline-info btn-circle btn-lg btn-circle ml-2"><i class="fa fa-trash"></i> </button>
-        <button><i class="fa fa-edit"></i  > </button>
-        <button type="button" class="btn btn-outline-info btn-circle btn-lg btn-circle ml-2"><i class="fa fa-upload"></i> </button>
-      </td>
-    </tr>
-      );
-  
-  
-    });
+    
+      return userProfiles.map((userProfiles, index) => {
+    
+        return (
+          <tr>
+        <td class="pl-4">{index+1}</td>
+        <td>
+            <h5 class="font-medium mb-0">{userProfiles.name} {userProfiles.surname}</h5>
+            <span>{userProfiles.address.street} {userProfiles.address.city} </span>
+        </td>
+        <td>
+            <span>{userProfiles.rank} </span><br></br>
+            
+        </td>
+        <td>
+            <span>{userProfiles.email}</span><br></br>
+            <span>{userProfiles.telephoneNumber} </span>
+        </td>
+        <td>
+            <span>{userProfiles.birthDate.split("T")[0].split("-")[2]}-{userProfiles.birthDate.split("T")[0].split("-")[1]}-{userProfiles.birthDate.split("T")[0].split("-")[0]}</span><br></br>
+            <span></span>
+        </td>
+        <td>
+           <button type="button" class="btnPurc"><i class="fa fa-list-alt"></i> </button>
+        </td>
+      </tr>
+        );
+    
+    
+      });
   }
-
+  //btn btn-outline-info btn-circle btn-lg btn-circle
 export default ApiComponent;
 
--- src/App.js
+++ src/App.js
@@ -2,38 +2,21 @@ import logo from './logo.svg';
 import ApiComponent from './ApiRequest.js';
 import './App.css';
 import './page.css';
+import "./userTable.css";
+import DefaultHeader from './header.js';
+import ViewPurchases from './viewPurchases';
 
 
 function App() {
+
   return (
     
     <div className="App" style={{backgroundColor: 'lightgray'}}>
      <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
 
-          <div class="headerContainer">
-            <table class="headerTable">
-              <thead class="homeVP">
-                <th>
-                  <a href="localhost:3000" ><img src="https://www.labaroviola.com/wp-content/uploads/2023/05/Francesco-Totti-instagram-24022023-grantennistoscana.it-2.jpg" class="fotoDerCapitano"/></a>
-                </th>
-                <th id="thSpacer"></th>
-                <th class="stdTh">
-                  Zubyr
-                </th>
-                <th>
-                  Zubbear
-                </th>
-                <th>
-                  Zuzzupets
-                </th>
-              </thead>
-              <tbody>
-
-              </tbody>
-            </table>
+         <DefaultHeader/>
 
 
-          </div>
           <div class="divSpacer"></div>
           <div class="container">
             <div class="row">
@@ -53,14 +36,11 @@ function App() {
                                   <th scope="col" class="border-0 text-uppercase font-medium">Role</th>
                                   <th scope="col" class="border-0 text-uppercase font-medium">Contacts</th>
                                   <th scope="col" class="border-0 text-uppercase font-medium">Date of Birth</th>
-                                  <th scope="col" class="border-0 text-uppercase font-medium">Category</th>
-                                  <th scope="col" class="border-0 text-uppercase font-medium">Manage</th>
+                                  <th scope="col" class="border-0 text-uppercase font-medium">View Purchases</th>
                                 </tr>
                               </thead>
 
-                              <tbody>
-                                <ApiComponent />
-                              </tbody>
+                              <tbody><ApiComponent /></tbody>
 
                           </table>
 
